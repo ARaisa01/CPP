ffe655d9f8672b63bc590f0b49ae9c565569894a
Afia Raisa <161967081+ARaisa01@users.noreply.github.com>
2024-06-28 22:11:31 +0600

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  int x,y,sum;
+
+  cout<<"Enter numbers:";
+  cin>>x>>y;
+
+  sum=x+y;
+  cout<<"the result is"<<sum<<'\n';
+
+
+  return 0;
+}
